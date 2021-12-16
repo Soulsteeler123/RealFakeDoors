c86f4d3baf87120ef6068739072a68555d7dd359
0x416C6578 <70234971+0x416C6578@users.noreply.github.com>
2021-12-16 02:00:36 -0500

    finalized sizebar behaviour

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sortbar = () => {
     return (
         <>
             <Card style={{marginTop: 10,marginRight: 10, minWidth: 150}}>
-                <div style={{marginTop: 10, marginLeft: 10}}>
+                <Grid style={{marginTop: 10, marginLeft: 10}}>
                     <FormControl component="fieldset">
                         <FormLabel component="legend">Door Size</FormLabel>
                         <RadioGroup aria-label="Door Size">
@@ -14,6 +14,8 @@ const Sortbar = () => {
                             <FormControlLabel value='30" x 80"' control={<Radio />} label='30" x 80"' />
                             <FormControlLabel value='36" x 80"' control={<Radio />} label='36" x 80"' />
                         </RadioGroup>
+                    </FormControl>
+                    <FormControl component="fieldset">
                         <FormLabel component="legend">Panels</FormLabel>
                         <RadioGroup aria-label="Panels">
                             <FormControlLabel value='1 Panel' control={<Radio />} label='1 Panel' />
@@ -21,13 +23,15 @@ const Sortbar = () => {
                             <FormControlLabel value='6 Panel' control={<Radio />} label='6 Panel' />
                             <FormControlLabel value='None' control={<Radio />} label='None' />
                         </RadioGroup>
+                    </FormControl>
+                    <FormControl component="fieldset">
                         <FormLabel component="legend">Core</FormLabel>
                         <RadioGroup aria-label="Core">
                             <FormControlLabel value='Hollow' control={<Radio />} label='Hollow' />
                             <FormControlLabel value='Solid' control={<Radio />} label='Solid' />
                         </RadioGroup>
                     </FormControl>
-                </div>
+                </Grid>
             </Card>
         </>
     )
